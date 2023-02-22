59082fff66f5267f908a3e8b12527a3cebfca451
OfficerF0X <114175604+OfficerF0X@users.noreply.github.com>
2023-02-22 17:44:24 +0100

    Add files via upload

---
+++ flaecheUmfangRechtwinkligesDreieck.py
@@ -0,0 +1,8 @@
+# Angabe: Fläche & Umfang eines rechtwinkeligen Dreieck berechnen
+
+a = int(input("Seite a: "))
+b = int(input("Seite b: "))
+c = int(input("Seite c: "))
+
+print("Umfang: ", a + b + c)
+print("Fläche: ", a * b / 2)
